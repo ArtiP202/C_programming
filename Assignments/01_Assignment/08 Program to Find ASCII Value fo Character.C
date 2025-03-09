d5fe0ef90d715aa38e32bc2660bff02c4bf31b7d
ArtiP202 <artipawar2125@gmail.com>
2025-03-09 20:33:05 +0530

    Add files via upload

---
+++ Assignments/01_Assignment/08 Program to Find ASCII Value fo Character.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch=0;
+
+    printf("\n ******ASCII TABLE******");
+
+    printf("\n Enter a Alphabate => ");
+    scanf("%c",&ch);
+
+    printf("\n The ASCII Code of %c is %d",ch,ch);
+
+    return 0;
+}
