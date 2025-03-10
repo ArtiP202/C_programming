fd67daa1cffd75979abae01c09f1a314dc0e17fa
ArtiP202 <artipawar2125@gmail.com>
2025-03-10 18:16:57 +0530

    ASCII Character Of Given Value

---
+++ Assignments/01_Assignment/09 Program to Find ASCII Character of Given Value.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+
+    printf("\n Enter ASCII value => ");
+    scanf("%d",&i);
+
+    printf("\n The Character for ASCII Value %d is %c ",i,i);
+
+   return 0;
+
+    }
