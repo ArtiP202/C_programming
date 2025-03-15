7cdd5efda6e15442dba8841e9b33000e8b1c0a03
ArtiP202 <artipawar2125@gmail.com>
2025-03-15 06:01:39 +0530

    Square Number

---
+++ Assignments/01_Assignment/17 Accept a Number and Calculate Squre of Number.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Number=0,Squre;
+
+    printf("\n Enter a Number => ");
+    scanf("%d",&Number);
+
+    Squre=Number*Number;
+
+    printf("\n Squre of Number is %d =%d",Number,Squre);
+
+    getch();
+    return 0;
+}
