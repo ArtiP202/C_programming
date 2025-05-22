0232af7cf62995d723609bb7a409e5d31a4632a0
ArtiP202 <artipawar2125@gmail.com>
2025-05-22 16:34:19 +0530

    Add files via upload

---
+++ Assignments/03_Assignment/01 A Program uses Loop Print Number in Incremnt Order.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+
+    printf("\n Increment ");
+
+    for(i=1;i<=10;i++)
+    {
+        printf("\n %d",i+100);
+    }
+    getch();
+    return 0;
+}
