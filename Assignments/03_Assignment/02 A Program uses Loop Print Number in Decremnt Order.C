40987326ae56c38f11ae770c61fc768e9ce484fb
ArtiP202 <artipawar2125@gmail.com>
2025-06-05 14:11:58 +0530

    Add files via upload

---
+++ Assignments/03_Assignment/02 A Program uses Loop Print Number in Decremnt Order.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+
+    printf("\n Numbers in Decrement Order");
+
+    for(i=10;i>=1;i--)
+    {
+        printf("\n %d",i);
+    }
+    getch();
+    return 0;
+}
