cb7e437835613c6d0a8b6979c5a03a50f70d536c
ArtiP202 <artipawar2125@gmail.com>
2025-06-05 14:12:32 +0530

    Add files via upload

---
+++ Assignments/03_Assignment/03 Accept a Number from User and Print that Number of Star.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+
+    printf("\n STAR \n\n");
+
+    for(i=1;i<=5;i++)
+    {
+        printf(" * ");
+    }
+    getch();
+    return 0;
+}
