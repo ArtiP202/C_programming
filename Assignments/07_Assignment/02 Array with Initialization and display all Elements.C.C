4c0aa55d8bcc480a1f69c99d5092c6a7535b2eed
ArtiP202 <artipawar2125@gmail.com>
2025-08-19 12:11:28 +0530

    Add files via upload

---
+++ Assignments/07_Assignment/02 Array with Initialization and display all Elements.C.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+  {
+      int iNum[5]={24,87,56,49,36};
+
+      printf("\n The 1st Element is = %d",iNum[0]);
+      printf("\n The 2nd Element is = %d",iNum[1]);
+      printf("\n The 3rd Element is = %d",iNum[2]);
+      printf("\n The 4th Element is = %d",iNum[3]);
+      printf("\n The 5th Element is = %d",iNum[4]);
+
+      getch();
+      return 0;
+  }
