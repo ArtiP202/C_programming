fc780688bfe46fbcc8aa699c6f43088de4f05f63
ArtiP202 <artipawar2125@gmail.com>
2025-08-21 18:02:04 +0530

    Add files via upload

---
+++ Assignments/07_Assignment/03 Array of 7 Elemnts with Null Initialization and display all elements .C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int iNum[7];
+
+    printf("\n The 1st Element is = %d",iNum[0]);
+    printf("\n The 2nd Element is = %d",iNum[1]);
+    printf("\n The 3rd Element is = %d",iNum[2]);
+    printf("\n The 4th Element is = %d",iNum[3]);
+    printf("\n The 5th Element is = %d",iNum[4]);
+    printf("\n The 6th Element is = %d",iNum[5]);
+    printf("\n The 7th Element is = %d",iNum[6]);
+
+    getch();
+    return 0;
+}
