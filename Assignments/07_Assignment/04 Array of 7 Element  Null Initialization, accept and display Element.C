e6f51f7fd1f9f90b62741c8eccb70c8c1cfdd729
ArtiP202 <artipawar2125@gmail.com>
2025-08-23 14:49:38 +0530

    Add files via upload

---
+++ Assignments/07_Assignment/04 Array of 7 Element  Null Initialization, accept and display Element.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int iNum[7];
+
+    printf("Enter 1st Number:");
+      scanf("%d",&iNum[0]);
+    printf("Enter 2nd Number:");
+      scanf("%d",&iNum[1]);
+    printf("Enter 3rd Number:");
+      scanf("%d",&iNum[2]);
+    printf("Enter 4th Number:");
+      scanf("%d",&iNum[3]);
+    printf("Enter 5th Number:");
+      scanf("%d",&iNum[4]);
+    printf("Enter 6th Number:");
+      scanf("%d",&iNum[5]);
+    printf("Enter 7th Number:");
+      scanf("%d",&iNum[6]);
+
+    getch();
+    return 0;
+
+}
