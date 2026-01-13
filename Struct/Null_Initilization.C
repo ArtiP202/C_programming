d974b41a87b531a3e9c0bddff407cfcc6de114d4
Arti Pawar <artipawar2125@gmail.com>
2026-01-13 17:42:10 +0530

    Add files via upload

---
+++ Struct/Null_Initilization.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Stud
+{
+    int RNo;
+    char Name[40];
+    long long int MobNo;
+    float per;
+};
+
+int main()
+{
+
+    struct Stud S1 = {};
+
+    printf("\n Student Details are => ");
+
+    printf("\n Roll No    : %d " ,S1.RNo);
+    printf("\n Name       : %s ", S1.Name);
+    printf("\n Mobile No  : %lld ", S1.MobNo);
+    printf("\n Percentage : %f " ,S1.per);
+
+    getch();
+
+    return 0;
+
+}
