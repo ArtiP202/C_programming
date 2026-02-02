b19abc049be721f174020a6125bb8f390a231059
Arti Pawar <artipawar2125@gmail.com>
2026-02-02 23:47:45 +0530

    Add files via upload

---
+++ Assignments/07_Assignment/09 Create,Accept ,Display Array using Loop-Find Count of given Element in that Array.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,Num[5]={0};
+
+    for(i=0;i<5;i++)
+    {
+        printf("Enter First Number=%d",i+1);
+        scanf("%d",&Num[i]);
+    if(Num[i] % 2=0)
+       {
+         printf("The Number is Even");
+       }
+      else
+       {
+         printf("The Number is Odd");
+       }
+    }
+
+
+    getch();
+    return 0;
+}
+
