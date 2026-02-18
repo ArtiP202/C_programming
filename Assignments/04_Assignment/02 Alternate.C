59b73f95c23061048e1f360bec2f86932350e9fe
Arti Pawar <artipawar2125@gmail.com>
2026-02-18 21:14:10 +0530

    Add files via upload

---
+++ Assignments/04_Assignment/02 Alternate.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,j=0,R=0,C=0;
+
+    printf("\n Enter Row Size => ");
+    scanf("%d",&R);
+    printf("\n Enter Column Size => ");
+    scanf("%d",&C);
+
+    for(i=1;i<=R;i++)
+    {
+        for(j=1;j<=C;j++)
+        {
+            if((i+j)%2==0)
+                printf(" * ");
+            else
+                printf(" # ");
+        }
+        printf("\n");
+    }
+ getch();
+ return 0;
+}
